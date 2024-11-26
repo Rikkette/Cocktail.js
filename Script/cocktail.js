877ffa8337bc78f3ac1d9f082de72ac07ab0c4ab
helene <darte.helene@gmail.com>
2024-11-26 16:56:37 +0000

    Cocktail.js

---
+++ Script/cocktail.js
@@ -0,0 +1,12 @@
+const API_BASE = "https://www.thecocktaildb.com/api/json/v1/1/";
+let button = document.getElementById("searchButton")
+let input=document.getElementById("searchInput")
+
+
+
+
+
+
+button.addEventListener('click', () => {
+    //boucle ?
+})
